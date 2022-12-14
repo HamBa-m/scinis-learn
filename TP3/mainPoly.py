15c3a4d15a1c020dcfcd426348c60557c0ff8dd5
HamBam <62408114+HamzaBamohammed@users.noreply.github.com>
2022-12-14 03:56:09 +0100

    adding polynomila regression and psy non linear transformer

--- TP3/mainPoly.py
+++ TP3/mainPoly.py
@@ -1,24 +1,31 @@
 from polynomial_regression import *
 import csv
 
-X, Y = [], []
+X1, X2, Y = [], [], []
 with open('pressure.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
-        X.append(np.array(float(row["temperature"])))
+        X1.append(np.array(float(row["temperature"])))
+        X2.append(np.array([float(row["temperature"])]))
         Y.append(np.array(float(row["pressure"])))
 
-X, Y = np.asarray(X), np.asarray(Y)
+X1, X2, Y = np.asarray(X1), np.asarray(X2), np.asarray(Y)
+
+# linear regression for polynomial tasks
+w0, ls = LinearRegressionforPoly(X1, Y, 5)
+print("FINAL RESULTS 1:")
+print("optimal weight vector: ", w0, "\t| empirical loss: ", "{0:.6f}".format(ls))
 
 # polynomial regression
-w0, ls = PolynmialRegression(X, Y, 5)
-print(w0, ls)
+w0, ls = PolynomialRegression(X2, Y, 5)
+print("FINAL RESULTS 2:")
+print("optimal weight vector: ", w0, "\t| empirical loss: ", "{0:.6f}".format(ls))
 
 import matplotlib.pyplot as plt
 
-plt.plot(X[:],Y,"or")
+plt.plot(X1[:],Y,"or")
 
-Z = [i for i in range(350)]
+Z = [i for i in range(370)]
 # hyperplan equation (in 2D)
 def h(wop,x):
     return sum([wop[i] * (x**i) for i in range(len(wop))])
@@ -27,4 +34,3 @@ hyper = [h(w0, e) for e in Z]
 plt.plot(Z,hyper,"-b" )
 
 plt.show()
-
