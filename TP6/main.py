b9055085c50d0e7af8f3c26f0c1b4edd028803c9
HamBam <62408114+HamzaBamohammed@users.noreply.github.com>
2023-01-05 03:03:38 +0100

    adding k-folds and spliting 80/20 methods

---
+++ TP6/main.py
@@ -0,0 +1,38 @@
+from k_fold import *
+from validation import *
+
+# data extraction
+data = []
+with open('temp_pre.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        X, Y = [], []
+        X.append(int(row["temperature"]))
+        Y.append(float(row["pressure"]))
+        data.append([X, Y])
+
+# validation using 1:5 ratio
+train, valid = validation(data)
+print("training set:\n",train)
+print("validation set\n", valid)
+print()
+
+with open("split.txt", "w") as output:
+    output.write("training set:\n")
+    for e in train:
+        output.write(str(e))
+        output.write("\n")
+    output.write("validation set:\n")
+    for e in valid:
+        output.write(str(e))
+        output.write("\n")
+
+# cross-validation using k-folds
+part = k_fold(data)
+for e in part:
+    print(e)
+
+with open("k_folds.txt", "w") as output:
+    for e in part:
+        output.write(str(e))
+        output.write("\n")
