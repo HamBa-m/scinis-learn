04d744aa91de0860e2e5001dae7abb2e47430871
HamBam <62408114+HamzaBamohammed@users.noreply.github.com>
2023-01-06 01:04:01 +0100

    adding logistic regression with regularization

--- TP7/main.py
+++ TP7/main.py
@@ -72,8 +72,17 @@ X_, Y_ = test[:,:10], test[:,10]
 #     print("generalization loss: ", "{0:.6f}".format(ld))
 #     print("|Ld - Ls| = ", "{0:.6f}".format(abs(ld - ls)))
 
+# # Elastic Net Regression
+# print("Elastic Net Regression:")
+# for lamda in [0.01,0.1,0.2,0.5,1,2]:
+#     w0, ls = LinearRegression(X, Y, reg="Lasso", lamda= lamda, lr=0.01)
+#     ld = loss(X_, Y_, w0, reg="Lasso", lamda=lamda)
+#     print("\nempirical loss: ", "{0:.6f}".format(ls),"\t | lamda ", lamda)
+#     print("generalization loss: ", "{0:.6f}".format(ld))
+#     print("|Ld - Ls| = ", "{0:.6f}".format(abs(ld - ls)))
+
 # Elastic Net Regression
-print("Elastic Net Regression:")
+print("Ridge Logistic Regression:")
 for lamda in [0.01,0.1,0.2,0.5,1,2]:
     w0, ls = LinearRegression(X, Y, reg="Lasso", lamda= lamda, lr=0.01)
     ld = loss(X_, Y_, w0, reg="Lasso", lamda=lamda)
