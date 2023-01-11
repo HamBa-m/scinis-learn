baa45cbf4b129f777d39283de4abdc63d3edbc4d
HamBam <62408114+HamzaBamohammed@users.noreply.github.com>
2023-01-11 21:52:48 +0100

    fix cross validation

--- LAB6/splitData.py
+++ LAB6/splitData.py
@@ -9,11 +9,12 @@ def split(data, p = 0.2):
         p: proportion of data in validation set
     return: training and validation sets
     """
-    training = list(data) # copy of data to avoid modifying data
+    training = np.copy(data) # copy of data to avoid modifying data
     validation = list() 
     m = len(data) 
     k = int(m * p) # number of data points in validation set
     for i in range(k):
         ind = randrange(len(training)) # choose a random index in training set 
-        validation.append(np.asarray(training.pop(ind))) # add the data point at the index to the validation set 
+        validation.append(training[ind]) # add the data point at the index to the validation set 
+        training = np.delete(training, ind, 0) # remove the data point at the index from the training set
     return np.asarray(training), np.asarray(validation)
